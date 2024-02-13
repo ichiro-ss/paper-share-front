7aa30a59901066daa654288724239b207319e92b
ichiro-ss <sei.swmr@gmail.com>
2024-02-13 23:26:08 +0900

    signup route

---
+++ src/pages/SignUp.jsx
@@ -0,0 +1,11 @@
+import { Header } from '../components/Header';
+export const SignUp = () => {
+  return (
+    <div>
+      <main className="signup">
+        <Header />
+        <h2>sign up</h2>
+      </main>
+    </div>
+  );
+};
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,10 +1,12 @@
 import { HashRouter, Route, Routes } from 'react-router-dom';
 import { Home } from '../pages/Home';
+import { SignUp } from '../pages/SignUp';
 
 export const Router = () => {
   return (
     <HashRouter>
       <Routes>
+        <Route path="/signup" element={<SignUp />} />
         <Route path="/" element={<Home />} />
       </Routes>
     </HashRouter>
