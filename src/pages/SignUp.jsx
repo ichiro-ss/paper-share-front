6507b2f8f08a3142ab1369fd24e01e8b51799735
ichiro-ss <sei.swmr@gmail.com>
2024-02-14 10:07:59 +0900

    updated signup

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -10,7 +10,7 @@ export const SignUp = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [errorMessage, setErrormessage] = useState();
-  const [cookies, setCookie] = useCookies();
+  const [cookies, setCookie, removeCookie] = useCookies();
   const {
     register,
     handleSubmit,
