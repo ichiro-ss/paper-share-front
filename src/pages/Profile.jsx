95fea5d6279f22158afdfcc84dcb9f48b12d6271
ichiro-ss <sei.swmr@gmail.com>
2024-02-14 10:21:30 +0900

    added profile path

---
+++ src/pages/Profile.jsx
@@ -0,0 +1,11 @@
+import { Header } from '../components/Header';
+
+export const Profile = () => {
+  return (
+    <div>
+      <main className="profile">
+        <Header />
+      </main>
+    </div>
+  );
+};
