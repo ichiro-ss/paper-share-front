74547233a672e1edd4786ebf7c081d6069f90a9b
ichiro-ss <sei.swmr@gmail.com>
2024-04-17 00:03:11 +0900

    add home link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,7 +13,10 @@ export const Header = () => {
     <header className="header">
       <div className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
         <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-          <Link className="flex items-center self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+          <Link
+            to="/"
+            className="flex items-center self-center text-xl font-semibold whitespace-nowrap dark:text-white"
+          >
             Share Papers
           </Link>
           <div className="flex items-center lg:order-2">
