0014b035a26206c7df8b95bcacc06eea7c6c1066
ichiro-ss <sei.swmr@gmail.com>
2024-04-16 20:41:44 +0900

    redesign header using tailwind

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,5 @@
 import { useCookies } from 'react-cookie';
 import { useNavigate, Link } from 'react-router-dom';
-import './header.scss';
 
 export const Header = () => {
   const navigate = useNavigate();
@@ -12,24 +11,41 @@ export const Header = () => {
   };
   return (
     <header className="header">
-      <h1>Share Papers</h1>
-      {cookies.token ? (
-        <>
-          <strong>{cookies.name}</strong>
-          <Link className="header__edit-user" to="/profile">
-            EDIT PROFILE
+      <div className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
+        <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
+          <Link className="flex items-center self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+            Share Papers
           </Link>
-          <button type="button" onClick={handleSignOut} className="sign-out-button">
-            SIGN OUT
-          </button>
-        </>
-      ) : (
-        <Link to="/signin">
-          <button type="button" className="sign-in-button">
-            SIGN IN
-          </button>
-        </Link>
-      )}
+          <div className="flex items-center lg:order-2">
+            {cookies.token ? (
+              <>
+                <Link
+                  to="/profile"
+                  className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
+                >
+                  {cookies.name}
+                </Link>
+                <button
+                  type="button"
+                  onClick={handleSignOut}
+                  className="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800"
+                >
+                  Sign out
+                </button>
+              </>
+            ) : (
+              <Link to="/signin">
+                <button
+                  type="button"
+                  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                >
+                  Sign in
+                </button>
+              </Link>
+            )}
+          </div>
+        </div>
+      </div>
     </header>
   );
 };
